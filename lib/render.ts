0d93489ea1c3c7513dcb46b482e05249de06114a
Keno Medenbach <keno@medenba.ch>
2021-07-01 20:41:22 +0200

    Support image cropping

--- lib/render.ts
+++ lib/render.ts
@@ -1,15 +1,32 @@
-import { decode } from 'imagescript/mod.ts'
+import { decode, GIF, Image } from 'imagescript/mod.ts'
 import { parse } from 'std/path/mod.ts'
 
 export async function renderSize(
   originalPath: string,
-  width: number,
-  height: number
+  newWidth: number,
+  newHeight: number
 ): Promise<Uint8Array> {
   const imageBytes = await Deno.readFile(originalPath)
-  const image = await decode(imageBytes)
+  const originalImage = await decode(imageBytes)
 
-  const resized = image.resize(width, height)
+  const originalRatioIsWider =
+    originalImage.width / originalImage.height > newWidth / newHeight
+
+  if (originalImage instanceof GIF) {
+    throw new Error('GIFs not supported.')
+  }
+
+  let resized: Image
+
+  // If the original image has a wider ratio than the new one, base the resize
+  // on the new height, else base it on the new width.
+  if (originalRatioIsWider) {
+    resized = originalImage.resize(Image.RESIZE_AUTO, newHeight)
+    resized.crop((resized.width - newWidth) / 2, 0, newWidth, newHeight)
+  } else {
+    resized = originalImage.resize(newWidth, Image.RESIZE_AUTO)
+    resized.crop(0, (resized.height - newHeight) / 2, newWidth, newHeight)
+  }
 
   if (!resized) {
     throw new Error('Error during image resize.')
